984149e6d8445bd9cb5d5420f86be7c4baa949df
Kacper <w.kacper.24@gmail.com>
2022-10-26 22:26:35 +0200

    Init

---
+++ Układanka/Classes/AnimationTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Układanka.Classes
+{
+    public static class AnimationTest
+    {
+        public static void ShowSimplePercentage()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.Write($"\rProgress: {i}%   ");
+                Thread.Sleep(25);
+            }
+
+            Console.Write("\rDone!          \n");
+        }
+    }
+}
---
+++ Układanka/Classes/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Układanka.Classes
+{
+    public class Game
+    {
+        public GameBoard Board { get; set; }
+        public Game(GameBoard board)
+        {
+            Board = board;
+        }
+    }
+}
